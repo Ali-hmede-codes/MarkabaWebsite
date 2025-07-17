6bd2ace6a3696db1da47eee884170a18e0afb704
Ali-hmede-codes <sowftwarestudio@gmail.com>
2025-07-17 22:04:16 +0300

    36

--- client/pages/category/[slug].tsx
+++ client/pages/category/[slug].tsx
@@ -160,7 +160,7 @@ const CategoryPage: React.FC = () => {
                   <article key={post.id} className="bg-white border border-gray-200 shadow-sm hover:shadow-md transition-shadow flex flex-col">
                     {/* Post Image */}
                     {post.featured_image && (
-                      <div className="w-full h-48 relative">
+                      <div className="w-full aspect-[4/3] relative">
                         <img
                           src={post.featured_image}
                           alt={post.title_ar || post.title}
