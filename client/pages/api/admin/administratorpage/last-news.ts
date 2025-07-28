80241e1b6a785459495b4ec4f40149f3f97b7457
Ali-hmede-codes <sowftwarestudio@gmail.com>
2025-07-28 12:21:56 +0300

    last news update v6

--- client/pages/api/admin/administratorpage/last-news.ts
+++ client/pages/api/admin/administratorpage/last-news.ts
@@ -14,7 +14,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     switch (method) {
       case 'GET':
         // Get all last news
-        const getResponse = await fetch(`${API_BASE_URL}/admin/administratorpage/last-news`, {
+        const getResponse = await fetch(`${API_BASE_URL}/api/admin/last-news`, {
           headers: {
             'Authorization': `Bearer ${token}`,
             'Content-Type': 'application/json'
@@ -25,7 +25,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
       case 'POST':
         // Create new last news
-        const postResponse = await fetch(`${API_BASE_URL}/admin/administratorpage/last-news`, {
+        const postResponse = await fetch(`${API_BASE_URL}/api/admin/last-news`, {
           method: 'POST',
           headers: {
             'Authorization': `Bearer ${token}`,
