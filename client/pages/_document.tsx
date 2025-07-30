53c9a82b1f37b6ca40447bc2622d31ea90c584ab
Ali-hmede-codes <sowftwarestudio@gmail.com>
2025-07-30 13:06:06 +0300

    faveicon update and manifest sitev1

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -15,9 +15,12 @@ export default function Document() {
         />
         
         {/* Favicons */}
-        <link rel="icon" href="/faveicon.png" type="image/png" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/favicon.svg" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
+        <link rel="icon" href="/android-chrome-192x192.png" sizes="192x192" type="image/png" />
+        <link rel="icon" href="/android-chrome-512x512.png" sizes="512x512" type="image/png" />
         <link rel="manifest" href="/site.webmanifest" />
         
         {/* Theme color for mobile browsers */}
