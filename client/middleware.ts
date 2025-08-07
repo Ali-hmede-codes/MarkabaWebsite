a148643baa0fb745b4d008e7b9a11307cc1c048f
Ali-hmede-codes <sowftwarestudio@gmail.com>
2025-08-07 22:19:02 +0300

    video posts v12

--- client/middleware.ts
+++ client/middleware.ts
@@ -20,7 +20,7 @@ export function middleware(request: NextRequest) {
     font-src 'self' https://fonts.gstatic.com;
     img-src 'self' blob: data: https: http:;
     media-src 'self' https:;
-    frame-src 'self' https://www.youtube.com https://youtube.com https://*.youtube.com;
+    frame-src 'self' https://www.youtube.com/embed/ https://youtube.com/embed/ https://*.youtube.com/embed/;
     connect-src 'self' https://www.google-analytics.com https://api.markaba.news http://localhost:5000;
     object-src 'none';
     base-uri 'self';
