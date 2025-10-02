eb0d16f97908189102473fe8bd59b046ccd23b22
Ali-hmede-codes <sowftwarestudio@gmail.com>
2025-10-02 18:35:37 +0300

    fix(_document): change OneSignal service worker paths to relative
    The absolute URLs were replaced with relative paths to ensure proper loading of service workers regardless of the deployment domain. This makes the configuration more flexible and deployment-independent.

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -184,8 +184,8 @@ class MyDocument extends Document<MyDocumentProps> {
                       safari_web_id: "web.onesignal.auto.4b99c5db-a7c9-461a-8333-facb0838095d",
                       allowLocalhostAsSecureOrigin: true,
                       serviceWorkerParam: { scope: '/' },
-                      serviceWorkerPath: 'https://www.markaba.news/OneSignalSDKWorker.js',
-                      serviceWorkerUpdaterPath: 'https://www.markaba.news/OneSignalSDKUpdaterWorker.js',
+                      serviceWorkerPath: '/OneSignalSDKWorker.js',
+                      serviceWorkerUpdaterPath: '/OneSignalSDKUpdaterWorker.js',
                       notificationClickHandlerMatch: 'origin',
                       notificationClickHandlerAction: 'navigate',
                       notifyButton: {
